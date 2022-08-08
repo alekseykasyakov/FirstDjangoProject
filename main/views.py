0264721fc681c273667193569f01ec7f6ad519e0
Aleksey <alekseykasyakov@gmail.com>
2022-08-08 02:08:17 -0700

    редактирование и вывод тасков

--- main/views.py
+++ main/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'Title':'Главная страница сайта','tasks':tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'форма была не верной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
